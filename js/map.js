49730c305c26291f765c383f441a62c00737b85a
Victoria Isaycheva <isaychevav@gmail.com>
2018-09-13 22:43:43 +0300

    Оживить интерактивные элементы в личном проекте
    Внесла все правки. Вопросов нет

---
+++ js/map.js
@@ -0,0 +1,22 @@
+var mapLink = document.querySelector(".contacts-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
--- js/popup.js
+++ js/popup.js
@@ -1,26 +1,55 @@
-var link = document.querySelector("button-feedback");
-var popup = document.querySelector("modal-feedback");
-var close = document.querySelector("modal-close");
+var link = document.querySelector(".button-feedback");
+var popup = document.querySelector(".modal-feedback");
+var close = popup.querySelector(".modal-close");
 var form = popup.querySelector("form");
-var name = popup.querySelector("[name=name]");
+var fullname = popup.querySelector("[name=fullname]");
 var mail = popup.querySelector("[name=e-mail]");
 var message = popup.querySelector("[name=message]");
-var storage = name.localStorage.getItem ("name");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("fullname");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  name.focus();
+
+  if (storage) {
+    fullname.value = storage;
+    mail.focus();
+  } else {
+    fullname.focus();
+  }
 });
 
- close.addEventListener("click", function (evt) {
-   evt.preventDefault();
-   popup.classList.remove("modal-show");
- });
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove('modal-error');
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+      popup.classList.remove('modal-error');
+    }
+  }
+});
 
 form.addEventListener("submit", function (evt) {
-   if (!login.value || !password.value) {
-     evt.preventDefault();
-     console.log("Нужно ввести логин и пароль");
- }
- });
+  if (!fullname.value || !mail.value || !message.value) {
+    evt.preventDefault();
+    popup.classList.add('modal-error');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("fullname", fullname.value);
+      localStorage.setItem("mail", mail.value);
+    }
+  }
+});
